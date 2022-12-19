ef60a0380560b5e9cdfc1c8ba49ae4ce466d3ea5
Nlsn <n911luna@gmail.com>
2022-12-18 20:12:41 -0600

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+
+    return (
+        <div className="wrapper">
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
+
+export default Main;
